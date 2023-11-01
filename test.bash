0a0f774dbf5c930b9e7dd32e8d9d14f8db7e30c3
Yuto Osawa <s22c1017np@s.chibakoudai.jp>
2023-11-01 12:00:46 +0900

    Improve the test

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash -xv
+# SPDX-FileCopyrightText: 2023 Yuto Osawa
+# SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng $LINENO
 
-[ "${out}" = 15 ]
+[ "$res" = 0 ] && echo OK
+exit $res
